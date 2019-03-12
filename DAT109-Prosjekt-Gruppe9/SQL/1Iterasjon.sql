f9b5a9d70d88cb6f87f284a839041d7a632fd228
H571531 <31210153+H571531@users.noreply.github.com>
2019-03-12 12:43:33 +0100

    Endret rekkefølgen på create tables.

--- DAT109-Prosjekt-Gruppe9/SQL/1Iterasjon.sql
+++ DAT109-Prosjekt-Gruppe9/SQL/1Iterasjon.sql
@@ -1,7 +1,20 @@
-DROP SCHEMA IF EXISTS ExpoSystem;
+DROP  SCHEMA IF EXISTS ExpoSystem CASCADE;
 CREATE SCHEMA ExpoSystem;
 SET search_path TO ExpoSystem;
 
+CREATE TABLE faculty(
+	facultyId VARCHAR(5),
+	facultyName VARCHAR(20),
+	CONSTRAINT facultyPK PRIMARY KEY (facultyId)
+);
+
+CREATE TABLE stand(
+    standId VARCHAR(20),
+	standName VARCHAR(20),
+	facultyId VARCHAR(5),
+    CONSTRAINT standPK PRIMARY KEY (standId),
+    CONSTRAINT facultyFK FOREIGN KEY (facultyId) REFERENCES faculty(facultyId)
+);
 CREATE TABLE vote(
     voteId SERIAL,
 	voteValue int,
@@ -10,25 +23,13 @@ CREATE TABLE vote(
     CONSTRAINT standFK FOREIGN KEY (standId) REFERENCES stand(standId)
 );
 
-CREATE TABLE stand(
-    standId VARCHAR(20),
-	standName VARCHAR(20),
-	fakultetId VARCHAR(5),
-    CONSTRAINT standPK PRIMARY KEY (standId),
-    CONSTRAINT facultyFK FOREIGN KEY (facultyId) REFERENCES faculty(facultyId)
-);
-
 CREATE TABLE admin(
 	adminId SERIAL,
 	username VARCHAR(20),
 	hashedPassword VARCHAR(88)
 );
 
-CREATE TABLE faculty(
-	facultyId VARCHAR(5),
-	facultyName VARCHAR(20),
-	CONSTRAINT facultyPK PRIMARY KEY (facultyId)
-);
+
 
 CREATE TABLE expo(
 	expoId SERIAL,
