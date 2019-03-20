2ad3ff82bdf3444b38d4aa3b8e8705567363deed
H571531 <31210153+H571531@users.noreply.github.com>
2019-03-20 09:47:55 +0100

    Admin og inlogging implementert.
    Peker til "AdminServlet"(Må implementeres)

--- DAT109-Prosjekt-Gruppe9/src/no/hvl/dat109/expo/entities/Admin.java
+++ DAT109-Prosjekt-Gruppe9/src/no/hvl/dat109/expo/entities/Admin.java
@@ -6,7 +6,7 @@ package no.hvl.dat109.expo.entities;
 import javax.persistence.*;
 
 /**
- * @author Mirnes
+ * @author 
  *
  */
 @Entity
@@ -14,71 +14,39 @@ import javax.persistence.*;
 public class Admin {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	int adminId;
+	private String username;
 	
-	String userName;
-	String hashedPassword;
+	private String hashedPassword;
 
 	public Admin() {
 	}
 
 	/**
 	 * Oppretter en ny admin
-	 * @param String userName
+	 * @param String username
 	 * @param String hashedPassord
 	 */
-	public Admin(String userName, String hashedPassord) {
-		this.userName = userName;
+	public Admin(String username, String hashedPassword) {
+		this.username = username;
 		this.hashedPassword = hashedPassword;
 	}
 
-	/**
-	 * Henter brukernavnet til en admin
-	 * @return String userName
-	 */
-	public String getUserName() {
-		return userName;
+	public String getUsername() {
+		return username;
 	}
 
-	/**
-	 * Gir en admin et nytt brukernavn
-	 * @param String userName det nye brukernavnet
-	 */
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
-	/**
-	 * Henter det hashede passordet til en admin
-	 * @return String hashedPassword passordet til en admin
-	 */
 	public String getHashedPassword() {
 		return hashedPassword;
 	}
 
-	/**
-	 * Gir en admin et nytt hashet passord
-	 * @param String hashedPassword det nye passordet
-	 */
 	public void setHashedPassword(String hashedPassword) {
 		this.hashedPassword = hashedPassword;
 	}
 
-	/**
-	 * Henter id til en admin
-	 * @return int adminId id til en admin
-	 */
-	public int getAdminId() {
-		return adminId;
-	}
 
-	/**
-	 * Gir en admin en ny id
-	 * @param int adminId den nye id'en
-	 */
-	public void setAdminId(int adminId) {
-		this.adminId = adminId;
-	}
 
 }
