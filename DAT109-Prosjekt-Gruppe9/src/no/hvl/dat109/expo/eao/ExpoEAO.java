0f4e798948507cfe2581c0f7196b064ac386ee66
tohov <tohov@158.37.224.134>
2019-03-27 10:35:25 +0100

    ExpoEAO: Javadoc

--- DAT109-Prosjekt-Gruppe9/src/no/hvl/dat109/expo/eao/ExpoEAO.java
+++ DAT109-Prosjekt-Gruppe9/src/no/hvl/dat109/expo/eao/ExpoEAO.java
@@ -6,16 +6,32 @@ import javax.persistence.PersistenceContext;
 
 import no.hvl.dat109.expo.entities.Expo;
 
+
+/**
+ * EAO for Expo
+ * @author Gruppe 9
+ *
+ */
 @Stateless
 public class ExpoEAO {
 	
 	@PersistenceContext(name = "expoPU")
 	private EntityManager em;
 	
+	/**
+	 * Finner en expo, gitt ved årstall
+	 * @param expoid Årstall for expo som skal finnes
+	 * @return Expo med gitt årtall hvis funnet, null hvis ikke funnet
+	 */
 	public Expo findExpo(String expoid) {
 		return em.find(Expo.class, expoid);
 	}
 
+	/**
+	 * Oppdaterer gitt Expo i database
+	 * @param expo Expo som skal oppdateres
+	 * @return Oppdatert Expo-objekt
+	 */
 	public Expo updateExpo(Expo expo) {
 		return em.merge(expo);
 		
